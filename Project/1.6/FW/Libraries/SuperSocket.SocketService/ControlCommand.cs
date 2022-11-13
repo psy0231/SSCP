dcd540317694b84bd68c041af101551bf3132849
psy_aw <psy0231@gmail.com>
2022-11-13 14:53:06 +0900

    work SuperSocket.SocketService
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/ControlCommand.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/MainService.Designer.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/MainService.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/Program.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/Properties/AssemblyInfo.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/SelfInstaller.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/SocketServiceInstaller.Designer.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/SocketServiceInstaller.cs
    	new file:   Project/1.6/FW/Libraries/SuperSocket.SocketService/SuperSocket.SocketService.csproj
    	modified:   SSCP.sln

---
+++ Project/1.6/FW/Libraries/SuperSocket.SocketService/ControlCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.SocketService
+{
+    class ControlCommand
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public Func<IBootstrap, string[], bool> Handler { get; set; }
+    }
+}
---
+++ Project/1.6/FW/Libraries/SuperSocket.SocketService/MainService.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceProcess;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
+
+namespace SuperSocket.SocketService
+{
+    partial class MainService : ServiceBase
+    {
+        private IBootstrap m_Bootstrap;
+
+        public MainService()
+        {
+            InitializeComponent();
+            m_Bootstrap = BootstrapFactory.CreateBootstrap();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (!m_Bootstrap.Initialize())
+            {
+                return;
+            }
+
+            m_Bootstrap.Start();
+        }
+
+        protected override void OnStop()
+        {
+            m_Bootstrap.Stop();
+            base.OnStop();
+        }
+
+        protected override void OnShutdown()
+        {
+            m_Bootstrap.Stop();
+            base.OnShutdown();
+        }
+    }
+}
